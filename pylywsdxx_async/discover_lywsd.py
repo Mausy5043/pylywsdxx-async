019211c45f1adbc96150a66aad2745119f8b973b
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-02-09 14:57:47 +0100

    typing and formatting updates

--- pylywsdxx_async/discover_lywsd.py
+++ pylywsdxx_async/discover_lywsd.py
@@ -5,30 +5,31 @@
 import asyncio
 
 from bleak import BleakScanner
+from bleak.backends.device import BLEDevice
 from bleak.exc import BleakDBusError
 
 
 async def discover() -> None:
     """Discover LYWSDxx devices and display them on the console."""
-    retry_attempts = 3
+    retry_attempts: int = 3
     for attempt in range(retry_attempts):
         try:
-            device_list = await BleakScanner().discover(timeout=60)
+            device_list: list[BLEDevice] = await BleakScanner().discover(timeout=60)
             for device in device_list:
                 if "LYWS" in str(device):
                     print(f"Address: {device.address} - Name: {device.name}.")
                 else:
                     print(f"-------  Name: {device.name}.")
             break
-        except BleakDBusError as e:
-            if "InProgress" in str(e):
-                print(f"Attempt {attempt + 1} failed: {e}. Retrying...")
+        except BleakDBusError as her:
+            if "InProgress" in str(her):
+                print(f"Attempt {attempt + 1} failed: {her}. Retrying...")
                 await asyncio.sleep(5)
             else:
-                raise e
+                raise her
 
 
-def main():
+def main() -> None:
     print("Scanning for LYWSDxx devices...(max 60s)")
     asyncio.run(discover())
 

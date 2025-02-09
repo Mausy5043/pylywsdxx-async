01d5e70765b970d055339c33c627e41739d03f2f
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-02-09 21:09:58 +0100

    should also work on Windows

--- examples/async_ex.py
+++ examples/async_ex.py
@@ -18,6 +18,9 @@ from pylywsdxx_async.device_async import Lywsd03  # noqa: E402
 
 
 # room 1.1
+MAC_ADDRESS_OR_UUID = ""
+if platform.system() == "Windows":
+    MAC_ADDRESS_OR_UUID = "A4:C1:38:6F:E7:CA"
 if platform.system() == "Darwin":
     MAC_ADDRESS_OR_UUID = "A05DCB20-0769-C0EB-BD4D-13A26C847B4A"
 if platform.system() == "Linux":
@@ -25,7 +28,7 @@ if platform.system() == "Linux":
 
 
 async def main(address) -> None:
-    async with Lywsd03(address, timeout=60) as client:
+    async with Lywsd03(address, notification_timeout=60) as client:
         lywsd03mmc_data = await client.get_data()
         print(lywsd03mmc_data)
 

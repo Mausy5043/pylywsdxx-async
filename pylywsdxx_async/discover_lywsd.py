0ccf729e091652ce01c6f4b00df30c2b06788f54
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-02-09 14:42:54 +0100

    avoid DBus error on Linux. Scanner appears not re-entrant

--- pylywsdxx_async/discover_lywsd.py
+++ pylywsdxx_async/discover_lywsd.py
@@ -5,17 +5,27 @@
 import asyncio
 
 from bleak import BleakScanner
+from bleak.exc import BleakDBusError
 
 
 async def discover() -> None:
     """Discover LYWSDxx devices and display them on the console."""
-    async with BleakScanner() as scanner:
-        device_list = await scanner.discover(timeout=60)
-        for device in device_list:
-            if "LYWS" in str(device):
-                print(f"Address: {device.address} - Name: {device.name}.")
+    retry_attempts = 3
+    for attempt in range(retry_attempts):
+        try:
+            device_list = await BleakScanner().discover(timeout=60)
+            for device in device_list:
+                if "LYWS" in str(device):
+                    print(f"Address: {device.address} - Name: {device.name}.")
+                else:
+                    print(f"-------  Name: {device.name}.")
+            break
+        except BleakDBusError as e:
+            if "InProgress" in str(e):
+                print(f"Attempt {attempt + 1} failed: {e}. Retrying...")
+                await asyncio.sleep(5)
             else:
-                print(f"-------  Name: {device.name}.")
+                raise e
 
 def main():
     print("Scanning for LYWSDxx devices...(max 60s)")
